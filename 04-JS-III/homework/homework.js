b6eacacde47da6883678231292dfae97d343dfeb
AMarcos96 <marcoosdiego@hotmail.com.ar>
2021-10-21 17:10:42 -0300

    Homework Finish

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -87,7 +87,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  return agregarNumeros(resultadosTest) / resultados.test
+  return agregarNumeros(resultadosTest) / resultadosTest.length;
 }
 
 
@@ -126,7 +126,8 @@ function cuentoElementos(arreglo){
     if(arreglo[i] > 19){
       contador++
     }
-}
+} 
+return contador
 }
 
 
@@ -135,7 +136,11 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if(numeroDeDia === 1 || numeroDeDia === 7 ){
+    return "Es fin de semana"
+  }
+  return "Es dia Laboral"
+
 } 
 
 
@@ -143,22 +148,23 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  if(numeroDeDia === 1 || numeroDeDia === 7 ){
-    return "Es fin de semana"
-  }
-  return "Es dia Laboral"
-}
-
-
-function todosIguales(arreglo) {
-  //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
-  //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
   let num = n.toString()
   if(num.charAt(0) === "9"){
     return true
   }
   return false
+}
+
+function todosIguales(arreglo) {
+  //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
+  //retornar true, caso contrario retornar false.
+  //Escribe tu código aquí  
+  for (var i =0 ; i < arreglo.length - 1; i++) {
+    if(arreglo[i] !== arreglo[i+1]){
+      return false
+    }
+  }
+  return true
 } 
 
 
